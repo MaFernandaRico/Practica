749d7518c2ccf33d6275a41261689de61dfd4b02
MaFernandaRico <maria.rico@cua.uam.mx>
2023-11-21 09:17:57 -0600

    creacion hola mundo

---
+++ holaMundo.py
@@ -0,0 +1,8 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre}!")
+    print(f"Tu número de matrícula es: {matricula}")
+
+if _name_ == "_main_":
+    nombre = "María Rico Arellano"
+    matricula = 2203070993
+    saludar(nombre, matricula)
